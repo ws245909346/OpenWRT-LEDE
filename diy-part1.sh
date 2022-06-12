7950b56159d9ecdb66d87fe9bc77336a3a7fde35
ws245909346 <102574232+ws245909346@users.noreply.github.com>
2022-06-12 11:48:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,11 +16,14 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+cd lean  
+rm -rf luci-theme-argon  
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 git clone https://github.com/kenzok8/openwrt-packages
 git clone https://github.com/kenzok8/small
 git clone https://github.com/esirplayground/luci-app-poweroff
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 git clone https://github.com/jerrykuku/luci-app-argon-config.git
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git
+cd ..
 sed -i 's/OpenWrt/OpenWRT_WSure_$(date "+%Y.%m.%d")/g' package/lean/default-settings/files/zzz-default-settings
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
